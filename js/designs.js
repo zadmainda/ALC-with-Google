a1a9166479a1c575f5f2e7fba77b1e5bb40b5939
zadock <kinara.zadock@gmail.com>
2018-06-30 16:50:33 +0300

    feat: add submit button event listener

---
+++ js/designs.js
@@ -0,0 +1,27 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+    // Your code goes here!
+    $("#pixelCanvas").html("");
+    const height = $("#inputHeight").val();
+    const width = $("#inputWeight").val();
+    for (i = 0; i < height; i++) {
+        $("#pixelCanvas").append("<tr></tr>");
+        for (j = 0; j < width; j++) {
+            $("#pixelCanvas")
+                .children()
+                .last()
+                .append("<td></td>");
+        }
+    }
+
+}
+
+$("form#sizePicker").on("submit", function(ev) {
+    ev.preventDefault();
+    makeGrid();
+});
